f25c49e0ce2806afc2214698d708097b7d033999
rishi9881 <rishichoudhary219881@gmail.com>
2025-09-01 11:22:17 +0530

    File Manager

---
+++ website/backend.js
@@ -0,0 +1,22 @@
+console.log("console log is used to display message on console to see console press F12");
+//Display Message//
+
+//window.alert("This is used to Display Alert box on web page");
+//Alert Box//
+
+document.getElementById("p1").textContent = "hello";
+/*In this document means main.html file , used id to call a tag .
+example <p id ="p1"> and textcontent means the text you want to show on your webpage.*/
+
+
+// let is used to store value 
+//This is declaration let x;
+//this is assignment x = 100;
+//type : number
+let age = 25;
+console.log(`hello I am ${age} years old hehe`);// All ways use `` this is under the esc button
+
+//type : string
+let name = "Rishi"
+console.log(`My name is ${name}`);
+
